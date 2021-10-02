80afaeba8b556b95374268e10a93e42985b0680f
Kit <epickitus@pm.me>
2021-10-03 00:30:12 +0300

    first

---
+++ src/routes/about.svelte
@@ -0,0 +1,4 @@
+<script>
+    console.log('hello from about page')
+</script>
+<h1>about page</h1>
---
+++ src/routes/index.svelte
@@ -0,0 +1,10 @@
+<script>
+    import { pokemon } from "../stores/pokestore"
+    console.log($pokemon)
+</script>
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+{#each $pokemon as pok}
+<h3 class="text-xl font-bold">{pok.name}</h3>
+{/each}
